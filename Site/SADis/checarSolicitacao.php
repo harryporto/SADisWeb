af580e11b99f3bb557feb7b5bf21183a0384f7ff
fkodanrc <fkodanrc@hotmail.com>
2014-11-14 21:11:17 -0300

    Criacao do webservice de acompanhamento

--- Site/SADis/checarSolicitacao.php
+++ Site/SADis/checarSolicitacao.php
@@ -1,58 +1,32 @@
-﻿<html>
-<head>
-	<meta charset="utf-8" />
-	<title>SADis - Acompanhar Solicitação</title>
-	<link rel="stylesheet" href="css/960_24_col.css" type='text/css'/> <!-- Grid 960 -->
-	<link rel="stylesheet" href="css/jquery.dataTables.css" type='text/css'/> <!-- Grid 960 -->
-	<link rel="stylesheet" href="css/style.css" type='text/css' /> 
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'><!-- GoogleFonts -->
-</head>
 <?php 
 
+	header('Content-Type: aplication/json; charset=utf-8');
+	
 	require "db.php";
-	$codigo = $_POST['CodSolic'];
-	
 
-	$result = mysql_query("SELECT StatusSolic FROM solicitacoes WHERE CodSolic ='$codigo' ");
-	
-	while($row = mysql_fetch_array($result)){
-		$status = utf8_encode($row["StatusSolic"]);
+	if (isset($_GET['CodSolic'])){
+		$codigo = mysql_real_escape_string($_GET['CodSolic']);
+
+	  $result = mysql_query("SELECT StatusSolic FROM solicitacoes WHERE CodSolic =".$codigo);
+	  if ($result){
+	    $jsonArray = array();
+	    while($row = mysql_fetch_array($result)){
+	      $jsonArray['data'][] = array(
+	        "date"    => utf8_encode($row["DateSolic"]),
+      	  "status"  => utf8_encode($row["StatusSolic"]),
+      	  "message" => utf8_encode($row["MsgSolic"])
+      	);
+	    }
+	  }
+  }	
+		
+	if (!isset($codigo) || !isset($jsonArray) || sizeof($jsonArray) == 0){
+	  $jsonArray['data'][] = array(
+	    "date"    => "",
+  	  "status"  => utf8_encode("Solicitação não encontrada"),
+  	  "message" => ""
+  	);
 	}
 	
-	if (!(isset($status))){
-		$status = "Solicitação não encontrada";
-	}
-	
+	echo json_encode($jsonArray);
 ?>
-
-<body>
-	<div class="background">
-		<div class="container_24">
-			<div class="grid_4 suffix_13">
-				<div class="logo">
-					<a href="index.html"><img src="logo_SADis_menor.png"></a>
-				</div>
-			</div>
-
-			<div class="grid_24">
-				<div class="background_transparente">    
-					<div class="id_aba_ativa">
-						Acompanhar Solicitação
-					</div>
-
-					<div class="clearfix"></div> 
-					<div class="background_conteudo">
-						<div class="background_conteudo">
-							<h2> Status da Solicitação: <br><?php echo $status;?></h2></br>
-							<a href="index.html">Retornar a página principal</a>
-							<div class="clearfix"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>  
-	</div>           
-</body>
-
-
-</html>
